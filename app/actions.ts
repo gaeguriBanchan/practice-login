582db01a871d2b13da7d42a8a186dd2cb236f463
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-08-14 18:13:52 +0900

    No blank space

--- app/actions.ts
+++ app/actions.ts
@@ -25,9 +25,13 @@ const formSchema = z
         invalid_type_error: '이름은 문자열이여야 합니다.',
         required_error: '이름을 입력 해주세요.',
       })
+      .trim()
       .min(3, '3글자 이상 입력하세요.')
       .max(10, '10글자 이하로 입력하세요.')
-      .refine(checkUsername, '욕설이 포함되면 안됩니다.'),
+      .refine(checkUsername, '욕설이 포함되면 안됩니다.')
+      .refine((value) => !/\s/.test(value), {
+        message: '사용자 이름에 공백이 포함될 수 없습니다.',
+      }),
     email: z.string().email('유효한 이메일 형식이 아닙니다.'),
     password: z.string().min(5, '비밀번호는 5글자 이상 작성하세요.'),
   })
